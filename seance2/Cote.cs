c271a4c94af813295eaa7b334d5e2cbd88b90973
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 10:25:23 +0200

    cote push

--- seance2/Cote.cs
+++ seance2/Cote.cs
@@ -2,5 +2,20 @@
 
 public class Cote
 {
+    private int note;
+    public Cote(int note)
+    {
+        this.note = note;
+    }
+    public int Note
+    {
+        get {
+            return note;
+        }
+    }
+    public void SetNote(int note)
+    {
+        this.note = note;
+    }
 
 }
