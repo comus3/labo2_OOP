fe257e266d211d0f48cb333ef652b734b502f5f5
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 11:53:20 +0200

    test module on main and everything works!

--- seance2/Program.cs
+++ seance2/Program.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using System.Reflection.Metadata;
-
-namespace seance2
+﻿namespace seance2
 {
     class Program
     {
 
         static void Main(string[] args)
         {
-            
+            test();   
+        }
+        static void test()
+        {
+            Enseignant Lurking = new Enseignant(10000,"Quentin","Lurk1");
+            Enseignant DLH = new Enseignant(15000,"Quentin","Delhaye");
+
+            Activite Info = new Activite("INFO","INFO3000",Lurking,5);
+            Activite Elec = new Activite("ELEC ANAL","ANAL3000",DLH,5);
+            Activite ElecNum = new Activite("ELEC NUM","NUM3000",DLH,3);
+
+            List<Eval> evaluations = new List<Eval>{
+                new Appreciation("TB",Info),
+                new Appreciation("B",Elec),
+                new Cote(14,ElecNum)
+            };
+ 
+            Etudiant Baptiste = new Etudiant("Batou","Chouchou",evaluations);
+
+            Console.WriteLine(Baptiste.Bulletin());
         }
     }
 }
