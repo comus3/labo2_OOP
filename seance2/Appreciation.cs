bb394b1bfec1ff64f5de0fc3df5258a7712d7cea
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 10:32:53 +0200

    appreciation class pushing

--- seance2/Appreciation.cs
+++ seance2/Appreciation.cs
@@ -2,5 +2,21 @@
 
 public class Appreciation
 {
-
+    private string appreciation;
+    public Appreciation(string appreciation)
+    {
+        this.appreciation = appreciation;
+    }
+    public int Note()
+    {
+        if (appreciation == "A"){return 20;}else if (appreciation == "B"){return 16;}
+        else if (appreciation == "C"){return 12;}
+        else if (appreciation == "D"){return 8;}
+        else if (appreciation == "E"){return 4;}
+        else{return 0;}
+    }
+    public void setNote(string note)
+    {
+        appreciation = note;
+    }
 }
