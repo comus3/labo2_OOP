a4fd9ab5b03046643fb4dda1a3347fe960466f13
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 09:20:54 +0200

    Program squeleton

---
+++ seance2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace seance2
+{
+    class Person
+    {
+
+    }
+    class Etudiant
+    {
+
+    }
+    class Enseignant
+    {
+
+    }
+    class Activite
+    {
+
+    }
+    class Eval
+    {
+
+    }
+    class Cote
+    {
+
+    }
+    class Appreciation
+    {
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
