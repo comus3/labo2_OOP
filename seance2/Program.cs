69aa36a41376af6ad19a39fa6b3c8e9bdc014f6b
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 09:29:03 +0200

    main update

--- seance2/Program.cs
+++ seance2/Program.cs
@@ -3,40 +3,12 @@ using System.Reflection.Metadata;
 
 namespace seance2
 {
-    class Person
-    {
-
-    }
-    class Etudiant
-    {
-
-    }
-    class Enseignant
-    {
-
-    }
-    class Activite
-    {
-
-    }
-    class Eval
-    {
-
-    }
-    class Cote
-    {
-
-    }
-    class Appreciation
-    {
-
-    }
     class Program
     {
 
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
