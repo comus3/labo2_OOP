a977c9e4ed36311fe9a9a58c30fe178028571200
comus3 <come.plantin.carrenard@gmail.com>
2023-10-18 09:45:56 +0200

    person class

--- seance2/Person.cs
+++ seance2/Person.cs
@@ -2,5 +2,15 @@
 
 public class Person
 {
-
+    private string firstName;
+    private string lastName;
+    public Person(string firstName,string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public string displayName()
+    {
+        return firstName + " " + lastName;
+    }
 }
